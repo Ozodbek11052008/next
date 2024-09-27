7feba68d4b3437e482931b96fd8c03c8d2df2ff3
ozodbek0808 <ozodabduvoitov8@gmail.com>
2024-09-27 23:59:26 +0500

    first commit

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,35 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Bosh sahifa",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Bizni afzaliklar",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Biz  Haqimizda",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Izohlar",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 5,
+    title: "Aloqa va  Texnik  xizmat  korsatish",
+    path: "/",
+    newTab: false,
+  }
+];
+export default menuData;
